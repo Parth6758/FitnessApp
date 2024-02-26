3753f8b9e1939bbfc51ca8a8194fde575f7ee4f6
Parth9811 <parthkotadiya666@gmail.com>
2024-02-26 17:01:41 +0530

    Add project files.

---
+++ Admin.aspx
@@ -0,0 +1,164 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Admin.aspx.cs" Inherits="FitnessApp.Admin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            text-align: center;
+            font-size: large;
+        }
+        .auto-style2 {
+            width: 1187px;
+        }
+        .auto-style3 {
+            text-align: left;
+            width: 149px;
+        }
+        .auto-style5 {
+            text-align: left;
+            width: 149px;
+            height: 24px;
+        }
+        .auto-style8 {
+            text-align: left;
+            height: 27px;
+        }
+        .auto-style10 {
+            text-align: left;
+            width: 149px;
+            height: 26px;
+        }
+        .auto-style12 {
+            text-align: left;
+            height: 24px;
+            width: 987px;
+        }
+        .auto-style13 {
+            text-align: left;
+            height: 26px;
+            width: 987px;
+        }
+        .auto-style14 {
+            text-align: left;
+            width: 987px;
+        }
+        .auto-style15 {
+            text-align: left;
+            width: 149px;
+            height: 28px;
+        }
+        .auto-style16 {
+            text-align: left;
+            width: 987px;
+            height: 28px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="auto-style1">
+            <strong>Admin Page</strong><table border="1" class="auto-style2">
+                <tr>
+                    <td class="auto-style3">Name:</td>
+                    <td class="auto-style14">
+                        <asp:TextBox ID="Name" runat="server" Width="979px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style3">Email:</td>
+                    <td class="auto-style14">
+                        <asp:TextBox ID="Email" runat="server" OnTextChanged="TextBox2_TextChanged" Width="978px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style5">Password:</td>
+                    <td class="auto-style12">
+                        <asp:TextBox ID="Password" runat="server" OnTextChanged="TextBox2_TextChanged" Width="978px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style5">Confirm Password:</td>
+                    <td class="auto-style12">
+                        <asp:TextBox ID="cpass" runat="server" OnTextChanged="TextBox2_TextChanged" Width="978px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style3">Age:</td>
+                    <td class="auto-style14">
+                        <asp:TextBox ID="Age" runat="server" OnTextChanged="TextBox2_TextChanged" Width="980px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style3">City:</td>
+                    <td class="auto-style14">
+                        <asp:DropDownList ID="Ddlcity" runat="server">
+                            <asp:ListItem>NewYork</asp:ListItem>
+                            <asp:ListItem>Miami</asp:ListItem>
+                            <asp:ListItem>Austin</asp:ListItem>
+                            <asp:ListItem>Texas</asp:ListItem>
+                            <asp:ListItem>LasVegas</asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style3">Height:</td>
+                    <td class="auto-style14">
+                        <asp:TextBox ID="Height" runat="server" OnTextChanged="TextBox2_TextChanged" Width="979px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style15">Weight:</td>
+                    <td class="auto-style16">
+                        <asp:TextBox ID="Weight" runat="server" OnTextChanged="TextBox2_TextChanged" Width="977px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style3">Gender:</td>
+                    <td class="auto-style14">
+                        <asp:RadioButtonList ID="Rblgender" runat="server" RepeatDirection="Horizontal">
+                            <asp:ListItem>Male</asp:ListItem>
+                            <asp:ListItem>Female</asp:ListItem>
+                            <asp:ListItem>Other</asp:ListItem>
+                        </asp:RadioButtonList>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style10">Course:</td>
+                    <td class="auto-style13">
+                        <asp:DropDownList ID="Ddlcourse" runat="server">
+                            <asp:ListItem>Full-Body</asp:ListItem>
+                            <asp:ListItem>Upper-Body</asp:ListItem>
+                            <asp:ListItem>Lower-Body</asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style8" colspan="2">
+                        <asp:Button ID="Insert" runat="server" OnClick="Insert_Click" Text="Insert " />
+&nbsp;
+                        <asp:Button ID="Update" runat="server" OnClick="Update_Click" Text="Update " />
+                    &nbsp;
+                        <asp:Button ID="Delete" runat="server" OnClick="Delete_Click" Text="Delete " />
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <p>
+            &nbsp;</p>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateSelectButton="True" BackColor="White" BorderColor="White" BorderStyle="Ridge" BorderWidth="2px" CellPadding="3" CellSpacing="1" GridLines="None" OnSelectedIndexChanged="GridView1_SelectedIndexChanged1">
+            <FooterStyle BackColor="#C6C3C6" ForeColor="Black" />
+            <HeaderStyle BackColor="#4A3C8C" Font-Bold="True" ForeColor="#E7E7FF" />
+            <PagerStyle BackColor="#C6C3C6" ForeColor="Black" HorizontalAlign="Right" />
+            <RowStyle BackColor="#DEDFDE" ForeColor="Black" />
+            <SelectedRowStyle BackColor="#9471DE" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+            <SortedAscendingHeaderStyle BackColor="#594B9C" />
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+            <SortedDescendingHeaderStyle BackColor="#33276A" />
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Admin.aspx.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace FitnessApp
+{
+    public partial class Admin : System.Web.UI.Page
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+        public static int Id;
+
+        public void fnconnection()
+        {
+            try { 
+                String constr = ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+                conn = new SqlConnection(constr);
+                if (conn.State != ConnectionState.Open)
+                {
+                    conn.Open();
+                  //Response.Write("Connected");
+                }
+            }
+           catch( Exception ex ) {
+            
+            }
+         }
+    
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)
+            {
+                fnconnection();
+                
+            }
+            fnbindgridview();
+        }
+
+        protected void fnbindgridview()
+        {
+            try
+            {
+                fnconnection();
+                String qry = "select * from Userinfo";
+                cmd = new SqlCommand(qry, conn);
+                sda = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet(); 
+                sda.Fill(ds);
+
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+                conn.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        protected void insert()
+        {
+            try
+            {
+                fnconnection();
+                String qry = "insert into Userinfo (Name,Email,Password,C_password,Age,City,Height,Weight,Gender,Course) VALUES (@Name,@Email,@Password,@C_password,@Age,@City,@Height,@Weight,@Gender,@Course)";
+                cmd = new SqlCommand(qry, conn);
+                cmd.Parameters.AddWithValue("Name", Name.Text);
+                cmd.Parameters.AddWithValue("Email", Email.Text);
+                cmd.Parameters.AddWithValue("Password", Password.Text);
+                cmd.Parameters.AddWithValue("C_password", cpass.Text);
+                cmd.Parameters.AddWithValue("Age", Age.Text);
+                cmd.Parameters.AddWithValue("City", Ddlcity.Text);
+                cmd.Parameters.AddWithValue("Height", Height.Text);
+                cmd.Parameters.AddWithValue("Weight", Weight.Text);
+                cmd.Parameters.AddWithValue("Gender", Rblgender.Text);
+                cmd.Parameters.AddWithValue("Course", Ddlcourse.Text);
+
+                int res = cmd.ExecuteNonQuery();
+                if (res > 0)
+                {
+                    Response.Write("User Inserted");
+                }
+                else
+                {
+                    Response.Write("User Not Inserted");
+                }
+                conn.Close();
+                fnbindgridview();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void update()
+        {
+            try
+            {
+                fnconnection();
+                String qry = "update Userinfo SET Name = @Name, Email = @Email, Password = @Password, C_password = @C_password, Age = @Age, City = @City, Height = @Height, Weight = @Weight, Gender = @Gender, Course = @Course where Id = @Id";
+                cmd = new SqlCommand (qry, conn);
+                
+                cmd.Parameters.AddWithValue("Name", Name.Text);
+                cmd.Parameters.AddWithValue("Email", Email.Text);
+                cmd.Parameters.AddWithValue("Password", Password.Text);
+                cmd.Parameters.AddWithValue("C_password", cpass.Text);
+                cmd.Parameters.AddWithValue("Age", Age.Text);
+                cmd.Parameters.AddWithValue("City", Ddlcity.Text);
+                cmd.Parameters.AddWithValue("Height", Height.Text);
+                cmd.Parameters.AddWithValue("Weight", Weight.Text);
+                cmd.Parameters.AddWithValue("Gender", Rblgender.Text);
+                cmd.Parameters.AddWithValue("Course", Ddlcourse.Text);
+                cmd.Parameters.AddWithValue("Id", Id);
+
+                int res = cmd.ExecuteNonQuery();
+                if(res > 0)
+                {
+                    Response.Write("User Updated");
+                }
+                else
+                {
+                    Response.Write("User not Updated");
+                }
+                conn.Close ();
+                fnbindgridview();
+            }
+            catch (Exception e)
+            {
+
+                Response.Write(e.ToString());
+            }
+        }
+
+        public void delete()
+        {
+            try
+            {
+                fnconnection();
+                String qry = "DELETE FROM Userinfo WHERE Id = @Id";
+                cmd = new SqlCommand(qry, conn);
+                cmd.Parameters.AddWithValue("Id",Id);
+                int res = cmd.ExecuteNonQuery();
+                if (res > 0)
+                {
+                    Response.Write("Data Removed");
+                }
+                else
+                {
+                    Response.Write("Not deleted");
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        protected void Insert_Click(object sender, EventArgs e)
+        {
+            insert();
+        }
+
+        protected void GridView1_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+            GridViewRow rw = GridView1.SelectedRow;
+            Id = Convert.ToInt32(rw.Cells[1].Text);
+            Name.Text = rw.Cells[2].Text;
+            Email.Text = rw.Cells[3].Text;
+            Password.Text = rw.Cells[4].Text;
+            cpass.Text = rw.Cells[5].Text;
+            Age.Text = rw.Cells[6].Text;
+            //   Ddlcity.Text = rw.Cells[7].Text;
+            Height.Text = rw.Cells[8].Text;
+            Weight.Text = rw.Cells[9].Text;
+            //    Rblgender.Text = rw.Cells[10].Text;
+            //    Ddlcourse.Text = rw.Cells[11].Text;
+
+            for (int i = 0; i < Ddlcity.Items.Count; i++)
+            {
+                if (Ddlcity.Items[i].Text == rw.Cells[7].Text.Trim())
+                {
+                    Ddlcity.SelectedIndex = i;
+                    
+                    break;
+
+                }
+            }
+            for (int i = 0; i < Rblgender.Items.Count; i++)
+            {
+                if (Rblgender.Items[i].Text == rw.Cells[10].Text.Trim())
+                {
+                    Rblgender.Items[i].Selected = true;
+                    break;
+                }
+            }
+            for (int i = 0; i < Ddlcourse.Items.Count; i++)
+            {
+                if (Ddlcourse.Items[i].Text == rw.Cells[11].Text.Trim())
+                {
+                    Ddlcourse.SelectedIndex = i;
+                    break;
+                }
+            }
+        }
+
+        protected void Update_Click(object sender, EventArgs e)
+        {
+            update();
+        }
+
+        protected void Delete_Click(object sender, EventArgs e)
+        {
+            delete();
+        }
+    }
+}
