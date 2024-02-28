0825ed92d55d42809a34b6b9acdd35136dd6f621
Parth9811 <parthkotadiya666@gmail.com>
2024-02-28 20:35:11 +0530

    Modal set for every course

--- CourseDetails.aspx
+++ CourseDetails.aspx
@@ -14,33 +14,26 @@
             <h1>Course Details</h1>
             <div id="courseContainer" runat="server" class="row">
                 <!-- Cards will be dynamically added here -->
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                    Launch
-                </button>
                 <asp:Literal ID="litCourseCards" runat="server"></asp:Literal>
             </div>
-
         </div>
 
-
-
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <!-- Modal for Launch Button -->
+        <div class="modal fade" id="launchModal" tabindex="-1" role="dialog" aria-labelledby="launchModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-scrollable" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title fs-5" id="exampleModalLabel">Modal title</h5>
+                        <h5 class="modal-title fs-5" id="launchModalLabel">Launch</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
                     <div class="modal-body">
-                        <!-- Modal body content here -->
-                        ...
+                        <p>Launch modal content goes here.</p>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <!-- Additional buttons here if needed -->
                     </div>
                 </div>
             </div>
@@ -49,6 +42,4 @@
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-</body>
-</html>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmY
--- CourseDetails.aspx.cs
+++ CourseDetails.aspx.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Data.SqlClient;
 using System.Data;
-using System.Linq;
-using System.Web;
+using System.Data.SqlClient;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Data.Common;
 
 namespace FitnessApp
 {
@@ -17,13 +13,13 @@ namespace FitnessApp
         SqlCommand cmd;
         SqlDataAdapter sda;
         public static int Id;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-             if (!IsPostBack)
+            if (!IsPostBack)
             {
                 DisplayCourses();
             }
-            
         }
 
         public void fnConnection()
@@ -34,8 +30,7 @@ namespace FitnessApp
                 conn = new SqlConnection(constr);
                 if (conn.State != ConnectionState.Open)
                 {
-                    
-                    Response.Write("Connected");
+                    //   Response.Write("Connected");
                 }
             }
             catch (Exception ex)
@@ -63,28 +58,64 @@ namespace FitnessApp
             }
 
         }
+
         protected void DisplayCourses()
         {
             DataSet dsCourses = GetAllCourses();
+            int cardCounter = 0;
             foreach (DataRow row in dsCourses.Tables[0].Rows)
             {
                 string courseName = row["C_Name"].ToString();
                 string courseDesc = row["Desc"].ToString();
 
                 string cardHtml = $@"
-        <div class='col-md-4'>
-            <div class='card'>
-                <div class='card-body'>
-                    <h5 class='card-title'>{courseName}</h5>
-                    <p class='card-text'>{courseDesc}</p>
-                    <p class='card-text'><small class='text-muted'>Last updated 3 mins ago</small></p>
+            <div class='col-md-4'>
+                <div class='card'>
+                    <div class='card-body'>
+                        <h5 class='card-title'>{courseName}</h5>
+                        <p class='card-text'>{courseDesc}</p>
+                        <button type='button' class='btn btn-primary' data-toggle='modal' data-target='#exampleModal{cardCounter}'>
+                            Launch
+                        </button>
+                        <p class='card-text'><small class='text-muted'>Last updated 3 mins ago</small></p>
+                    </div>
                 </div>
-            </div>
-        </div>";
+            </div>";
+
                 // Append the HTML to the Literal control
                 litCourseCards.Text += cardHtml;
-            }
 
+                // Modal HTML for each card
+                string modalHtml = $@"
+            <!-- Modal for card {cardCounter} -->
+            <div class='modal fade' id='exampleModal{cardCounter}' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+                <div class='modal-dialog modal-dialog-scrollable' role='document'>
+                    <div class='modal-content'>
+                        <div class='modal-header'>
+                            <h5 class='modal-title fs-5'>{courseName}</h5>
+                            <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+                                <span aria-hidden='true'>&times;</span>
+                            </button>
+                        </div>
+                        <div class='modal-body'>
+                            <p>{courseDesc}</p>
+                            <!-- Additional modal content here if needed -->
+                        </div>
+                        <div class='modal-footer'>
+                            <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>
+                            <!-- Additional buttons here if needed -->
+                        </div>
+                    </div>
+                </div>
+            </div>";
+
+                // Append the modal HTML to the form
+                form1.Controls.Add(new LiteralControl(modalHtml));
+
+                cardCounter++;
+            }
         }
+
+
     }
 }
